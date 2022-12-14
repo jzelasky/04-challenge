4c1869db726b6ac5431a0209471ecd50d86b6db3
Jessica Zelasky <jzelasky@gmail.com>
2022-12-14 08:53:16 -0500

    added timer and score functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,12 @@ var answerButton1 = document.getElementById("answerButton1");
 var answerButton2 = document.getElementById("answerButton2");
 var answerButton3 = document.getElementById("answerButton3");
 var answerButton4 = document.getElementById("answerButton4");
+var timerEl = document.getElementById("timer");
+var scoreEl = document.getElementById("score");
+
+var timer;
+var timerCount;
+var score = 0;
 
 function init(){
     answerButton1.hidden = true;
@@ -52,9 +58,11 @@ function init(){
 
 init();
 
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startQuiz);
 
-function startGame(){
+function startQuiz(){
+    timerCount = 60;
+    startTimer();
     startButton.hidden =true;
     answerButton1.hidden = false;
     answerButton2.hidden = false;
@@ -65,5 +73,66 @@ function startGame(){
     answerButton2.textContent = questions.question1.answerIncorrect1;
     answerButton3.textContent = questions.question1.answerIncorrect2;
     answerButton4.textContent = questions.question1.answerIncorrect3;
+
+    //if user clicks on correct answer add 1 to score
+    //if user click on incorrect answer subtract from timer
 }
 
+answerButton1.addEventListener("click", function(){
+    score++;
+    if (score > 1){
+        scoreEl.textContent = score + " points";  
+    } else if (score =1){
+        scoreEl.textContent = score + " point";
+    }
+    endQuiz();
+})
+
+answerButton2.addEventListener("click",function(){
+    if (timerCount > 15){
+        timerCount = timerCount -15;
+    } else if(timerCount <= 15){
+        endQuiz();
+    }
+})
+
+answerButton3.addEventListener("click",function(){
+    if (timerCount > 15){
+        timerCount = timerCount -15;
+    } else if(timerCount <= 15){
+        endQuiz();
+    }
+})
+
+answerButton4.addEventListener("click",function(){
+    if (timerCount > 15){
+        timerCount = timerCount -15;
+    } else if(timerCount <= 15){
+        endQuiz();
+    }
+})
+
+function startTimer(){
+    timer = setInterval(function(){
+        timerCount--;
+        timerEl.textContent = timerCount + " seconds left";
+        if(timerCount === 0){
+            endQuiz();
+        }
+    }, 1000)
+}
+
+function endQuiz(){
+    timerEl.textContent = "Quiz finished"
+    if (score > 1 || score === 0){
+        scoreEl.textContent = score + " points";  
+    } else if (score =1){
+        scoreEl.textContent = score + " point";
+    }
+    clearInterval(timer);
+    answerButton1.hidden = true;
+    answerButton2.hidden = true;
+    answerButton3.hidden = true;
+    answerButton4.hidden = true;
+    questionDisplay.hidden = true;
+}
