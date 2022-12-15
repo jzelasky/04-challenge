09f452013c7bd15f08534de26ae2854fcf297917
Jessica Zelasky <jzelasky@gmail.com>
2022-12-15 10:49:28 -0500

    added score to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 /*
 T0-DO:
--Make the submit form for saving scores work 
 -Make the view highscores button bring up saved scores
 -M
 */
@@ -37,7 +36,10 @@ var answerButton3 = document.getElementById("answerButton3");
 var answerButton4 = document.getElementById("answerButton4");
 var timerEl = document.getElementById("timer");
 var scoreEl = document.getElementById("score");
-var nameEl = document.getElementById("name");
+var nameInput = document.getElementById("nameInput");
+var highscoreBtn = document.getElementById("highscores");
+var highscoreHeader = document.querySelector("h3");
+var scoreDisplay = document.querySelector("h4");
 
 var timer;
 var timerCount;
@@ -49,7 +51,7 @@ function init(){
     answerButton2.hidden = true;
     answerButton3.hidden = true;
     answerButton4.hidden = true;
-    nameEl.hidden = true;
+    nameInput.hidden = true;
 }
 
 init();
@@ -215,19 +217,32 @@ function endQuiz(){
     answerButton3.hidden = true;
     answerButton4.hidden = true;
     questionDisplay.hidden = true;
-    nameEl.hidden = false 
+    nameInput.hidden = false 
 }
 
-
-//SAVE SCORES - DOESN'T WORK
-nameEl.addEventListener("submit", function(event){
+document.querySelector('form').addEventListener('submit', function(event){
     event.preventDefault();
-    var nameText = nameEl.value.trim();
-    scoreSaved.push(score + nameText);
-    saveScore();
-    console.log(scoreSaved);
+    if (score === 1){
+        var nameAndScore = nameInput.value + ": " + score + " point";
+    } else{
+        var nameAndScore = nameInput.value + ": " + score + " points";
+    }
+    localStorage.setItem("nameAndScore", JSON.stringify(nameAndScore));
+    displayScores();
 })
 
-function saveScore(){
-    localStorage.setItem("score", JSON.stringify(scoreSaved));
+highscoreBtn.addEventListener("click", displayScores);
+
+function displayScores(){
+    answerButton1.hidden = true;
+    answerButton2.hidden = true;
+    answerButton3.hidden = true;
+    answerButton4.hidden = true;
+    nameInput.hidden = true;
+    startButton.hidden = true;
+    highscoreBtn.hidden = true;
+    timerEl.hidden = true;
+    scoreEl.hidden = true;
+    highscoreHeader.textContent = "Highscores:"
+    scoreDisplay.textContent = JSON.parse(localStorage.getItem('nameAndScore'));
 }
